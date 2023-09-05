01891195fa154469958ccb44967d3805c474c3e3
CambiG1123 <campbellgilliland@gmail.com>
2023-09-05 16:19:24 -0400

    created the questions for the inquirer prompts

---
+++ index.js
@@ -0,0 +1,32 @@
+const fs = require('fs')
+const inquirer = require('inquirer')
+const { default: CheckboxPrompt } = require('inquirer/lib/prompts/checkbox')
+
+
+
+const questions = [
+    {
+        type: "Checkbox",
+        name: "shape",
+        message:"Please select what shape you would like your logo to be.",
+        choices: ['Circle','Triangle',"Square"]
+    },
+    {
+        type: 'input',
+        name: 'BG-color',
+        message: 'What color would you like the background of your shape to be? Type the name of a color or a hexadecimal code.'
+
+        
+    },
+    {
+        type: 'input',
+        name: 'text',
+        message: 'What would like your logo to say? (Please choose 3 letters).'
+
+    },
+    {
+        type: 'input',
+        name: 'text-color',
+        message: 'What color would you like your text to be? Type the name of a color or a hexadecimal code.'
+    }
+]
