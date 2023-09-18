711eee05c34132e2d9541b19d2b4967588f795a8
CambiG1123 <campbellgilliland@gmail.com>
2023-09-18 16:11:00 -0400

    created conditional for rendering either square,triangle, or circle

--- index.js
+++ index.js
@@ -1,20 +1,46 @@
 import inquirer from 'inquirer'
-// import fs from 'fs'
-// import { Triangle, Square, Circle } from "./lib/shapes"
+import fs from 'fs'
+import Shapes from "./lib/shapes.js";
 
+const { Triangle, Square, Circle } = Shapes;
 
+function writeToFile(fileName, responses){
+    // initiate the file as an empty string
+    let svgInput = ''
+//    sets the height and width of the logo container
+    svgInput = 
+    '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">'
+    svgInput += '<g>'
+    svgInput += `${responses.shape}`
+    
+    // if else that checks input from the questions and adds to the svgInput string
+    let shapeChoice
+    if (responses.shape == 'Square'){
+        shapeChoice = new Square()
+        svgInput += `<rect x="73" y="40" width="160" height="160" fill="${responses.bgColor}"/>`
+    } else if (responses.shape == 'Circle'){
+        shapeChoice = new Circle()
+        svgInput += `<circle cx="150" cy="115" r="80" fill="${responses.bgColor}"/>`
+    } else if (responses.shape == 'Triangle'){
+        shapeChoice = new Triangle()
+        svgInput += `<polygon points="150, 18 244, 182 56, 182" fill="${responses.bgColor}"/>`
+    }
+    
+    
+
+}
 
 
 const questions = [
     {
-        type: "Checkbox",
+        type: "checkbox",
         name: "shape",
         message:"Please select what shape you would like your logo to be.",
-        choices: ['Circle','Triangle',"Square"]
+        choices: ['Circle','Triangle','Square']
     },
     {
         type: 'input',
-        name: 'BG-color',
+        name: 'bgColor',
         message: 'What color would you like the background of your shape to be? Type the name of a color or a hexadecimal code.'
 
         
@@ -27,7 +53,7 @@ const questions = [
     },
     {
         type: 'input',
-        name: 'text-color',
+        name: 'textColor',
         message: 'What color would you like your text to be? Type the name of a color or a hexadecimal code.'
     }
 ]
